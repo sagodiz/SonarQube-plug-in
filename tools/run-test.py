ccb1270f3a28b99db6cc69eda84d63b5bf9d4046
Ságodi Zoltán <sagodiz@inf.u-szeged.hu>
2021-02-09 16:32:30 +0100

    Add functional testint. Getting SM missing so it will fail now. Maybe in actions it could be done

--- tools/run-test.py
+++ tools/run-test.py
@@ -36,6 +36,10 @@ import subprocess
 import zipfile
 from time import sleep
 import platform
+import pycurl
+from urllib.parse import urlencode
+from io import BytesIO
+import json
 
 import common
 
@@ -43,9 +47,9 @@ def get_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('--init', action='store_true', default=False,
                         help='Initialize')
-    parser.add_argument('-sv', '--server-version', default='7.2',
+    parser.add_argument('-sv', '--server-version', default='7.9.5',
                         help='Version of the SQ server (default=%(default)s)')
-    parser.add_argument('-scv', '--scanner-version', default='3.2.0.1227',
+    parser.add_argument('-scv', '--scanner-version', default='4.5.0.2216',
                         help='Version of the SQ scanner (default=%(default)s)')
     parser.add_argument('-plgf', '--plugins-folder', default=None,
                         help='Folder containing the plugins to be tested (default=%(default)s)')
@@ -57,6 +61,12 @@ def get_arguments():
                         help='Wait between checking the status of SonarQube server (default=%(default)s seconds)')
     parser.add_argument('-cf', '--client-folder', default='test',
                         help='Folder for downloading and unzipping SQ server and scanner (default=%(default)s)')
+    parser.add_argument('-smd', '--sourcemeter-directory', default='test',
+                        help='Folder for downloading and unzipping SourceMeter (default=%(default)s)')
+    parser.add_argument('-tpn', '--test-project-name', default='test',
+                        help='Name of teh project during testing. (default=%(default)s)')
+    parser.add_argument('-lot', '--language-of-test', default='java',
+                        help='Language of the test. (default=%(default)s)')
     parser.add_argument('--print-log', action='store_true',
                         help='Prints the SQ log files to the screen')
 
@@ -152,19 +162,156 @@ def validate_running_of_sq_server(version, number_of_attempts, wait):
             sleep(float(wait))
     return False
 
-def analyze(scanner_version, project_folder, system, dst):
-    cmd = ''
-    cwd = os.getcwd()
-    scanner_location =  ''
+def create_user_in_sq(username = 'myuser', password = 'mypassword'):
+
+    crl = pycurl.Curl()
+    #crl.setopt(pycurl.HTTPAUTH, pycurl.HTTPAUTH_DIGEST)
+    crl.setopt(crl.URL, 'http://127.0.0.1:9000/api/users/create')
+    crl.setopt(pycurl.USERPWD, 'admin:admin')
+    data = [('login', username),
+    ('password', password),
+    ('password_confirmation', password),
+    ('name', 'name'),
+    ('email', 'mail@email.com'),]
+    pf = urlencode(data)
+
+    # Sets request method to POST,
+    # Content-Type header to application/x-www-form-urlencoded
+    # and data to send in request body.
+    crl.setopt(crl.POSTFIELDS, pf)
+    crl.perform()
+
+    code = crl.getinfo(pycurl.HTTP_CODE)
+    print(code)
+    crl.close()
+
+    crl = pycurl.Curl()
+    #crl.setopt(pycurl.HTTPAUTH, pycurl.HTTPAUTH_DIGEST)
+    crl.setopt(crl.URL, 'http://127.0.0.1:9000/api/user_groups/add_user')
+    crl.setopt(pycurl.USERPWD, 'admin:admin')
+    data = [('login', username),
+    ('name', 'sonar-administrators'),]
+    pf = urlencode(data)
+    crl.setopt(crl.POSTFIELDS, pf)
+    crl.perform()
+
+    code_admin = crl.getinfo(pycurl.HTTP_CODE)
+    crl.close()
+    return (code == 200 and code_admin == 204) # 204 means OK
+
+def set_sm(toolchaindir, admin_user = 'myuser', admin_pwd = 'mypassword'):
+    crl = pycurl.Curl()
+    #crl.setopt(pycurl.HTTPAUTH, pycurl.HTTPAUTH_DIGEST)
+    crl.setopt(crl.URL, 'http://127.0.0.1:9000/api/settings/set')
+    crl.setopt(pycurl.USERPWD, f"{admin_user}:{admin_pwd}")
+    data = [('key', 'sm.toolchaindir'),
+            ('value', toolchaindir),]
+    pf = urlencode(data)
+    crl.setopt(crl.POSTFIELDS, pf)
+    crl.perform()
+
+    code = crl.getinfo(pycurl.HTTP_CODE)
+    crl.close()
+
+    return code == 204
+
+def set_default_profile(profile, language, admin_user = 'myuser', admin_pwd = 'mypassword'):
+    crl = pycurl.Curl()
+    #crl.setopt(pycurl.HTTPAUTH, pycurl.HTTPAUTH_DIGEST)
+    crl.setopt(crl.URL, 'http://127.0.0.1:9000/api/qualityprofiles/set_default')
+    crl.setopt(pycurl.USERPWD, f"{admin_user}:{admin_pwd}")
+    data = [('language', language),
+            ('qualityProfile', profile),]
+    pf = urlencode(data)
+    crl.setopt(crl.POSTFIELDS, pf)
+    crl.perform()
+
+    code = crl.getinfo(pycurl.HTTP_CODE)
+    crl.close()
+    
+    return code == 204 # 204 marks OK
+
+def create_project(project_name = 'test_project', admin_user = 'myuser', admin_pwd = 'mypassword'):
+    crl = pycurl.Curl()
+    #crl.setopt(pycurl.HTTPAUTH, pycurl.HTTPAUTH_DIGEST)
+    crl.setopt(crl.URL, 'http://127.0.0.1:9000/api/projects/create')
+    crl.setopt(pycurl.USERPWD, f"{admin_user}:{admin_pwd}")
+    data = [('name', project_name),
+            ('project', project_name),]
+    pf = urlencode(data)
+    crl.setopt(crl.POSTFIELDS, pf)
+    crl.perform()
+    crl.getinfo(pycurl.HTTP_CODE)
+    crl.close()
+    
+    crl = pycurl.Curl()
+    data_resp = BytesIO()
+    #crl.setopt(pycurl.HTTPAUTH, pycurl.HTTPAUTH_DIGEST)
+    crl.setopt(crl.URL, 'http://127.0.0.1:9000/api/user_tokens/generate')
+    crl.setopt(pycurl.USERPWD, f"{admin_user}:{admin_pwd}")
+    crl.setopt(pycurl.WRITEFUNCTION, data_resp.write)
+    data = [('name', project_name),]
+    pf = urlencode(data)
+    crl.setopt(crl.POSTFIELDS, pf)
+    crl.perform()
+    code_token = crl.getinfo(pycurl.HTTP_CODE)
+    crl.close()
+
+    token = None
+    if 200 == code_token:
+        response = json.loads(data_resp.getvalue())
+        token = response['token']
+
+    return token
+
+
+def analyze(dst, # folder of client (Where scanner is extracted.)
+            scanner_version, 
+            system, # system teh test runs on
+            abs_project_path, 
+            token, # token for the user to upload results
+            language = 'java', # analyzed project's language
+            binary_path = '', # if needed for the language like java
+            project_key = 'test_project', # select project
+            server_address = 'http://localhost:9000'):
+
+    scanner_binary = os.path.join(dst, f"sonar-scanner-{scanner_version}-{system}", "bin")
     if system == 'Windows':
-        scanner_location = cwd  + '\\' + dst + '\\sonar-scanner-%s-windows\\bin\\sonar-scanner.bat' % scanner_version
-        os.chdir(project_folder)
-        common.run_cmd('start', [scanner_location])
-    elif system == 'Linux':
-        cmd = cwd + '/' + dst + '/sonar-scanner-%s-linux/bin/sonar-scanner&' % scanner_version
-        os.chdir(project_folder)
-        common.run_cmd(cmd, [])
-    os.chdir('..')
+        # windows
+        scanner_binary += '\sonar-scanner.bat'
+    else:
+        # linux
+        scanner_binary += '/sonar-scanner.sh'
+
+    parameters = [f"-Dsonar.projectBaseDir={abs_project_path}", 
+                f"-Dsonar.projectKey={project_key}",
+                f"-Dsonar.host.url={server_address}"]
+    if language == 'java':
+        parameters.append(f"-Dsonar.java.binaries={binary_path}")
+
+    common.run_cmd(scanner_binary, parameters)
+
+def validate_results(project_name, language, admin_user = 'myuser', admin_pwd = 'mypassword'):
+    crl = pycurl.Curl()
+    data_resp = BytesIO()
+
+    parameters = f"?componentKey={project_name}&metricKeys="
+
+    if language == 'java':
+        parameters += "SM_JAVA_LOGICAL_LEVEL1"
+
+    crl.setopt(crl.URL, 'http://127.0.0.1:9000/api/measures/component' + parameters)
+    crl.setopt(pycurl.USERPWD, f"{admin_user}:{admin_pwd}")
+    crl.setopt(pycurl.WRITEFUNCTION, data_resp.write)
+    crl.perform()
+    crl.close()
+    data = json.loads(data_resp.getvalue())
+    data = json.loads(data['component']['measures'][0]['value'])
+    if 0 != data['level'][0]['metrics']['TLOC']:
+        return True
+
+    return False
+
 
 def print_log(version, path):
     path = os.path.join(path, 'sonarqube-%s' % version, 'logs')
@@ -183,14 +330,14 @@ def main(options):
     system = platform.system()
     dst = options.client_folder
     print_log_files = options.print_log
+    sm_directory = options.sourcemeter_directory
+    test_project_name = options.test_project_name
+    language_of_test = options.language_of_test
     common.mkdir(dst)
 
     # 0, a) Try to build the plugins with 'build.py'
-
-    if system == 'Windows':
-        common.run_cmd('py', ['-3', 'build.py', '--all'])
-    elif system == 'Linux':
-        common.run_cmd('python3', ['tools/build.py', '--all'])
+    
+    common.run_cmd('python3', ['tools/build.py', '--all'])
 
     if options.init == True:
         # 0, b) download sonar-server'
@@ -203,17 +350,15 @@ def main(options):
 
         # 2) unzip both server and scanner
 
-        src = os.path.join(dst, 'sonarqube-%s.zip' % server_version)
+        src = os.path.join(dst, f"sonarqube-{server_version}.zip")
         unzip(src, dst)
-        if 'Windows' == system:
-            src = os.path.join(dst, 'sonar-scanner-cli-%s-windows.zip' % scanner_version)
-        elif 'Linux' == system:
-            src = os.path.join(dst, 'sonar-scanner-cli-%s-linux.zip' % scanner_version)
+
+        src = os.path.join(dst, f"sonar-scanner-cli-{scanner_version}-{system.lower()}.zip")
         unzip(src, dst)
 
     # 3) copy the plugins into the server dir
 
-    path = [dst, 'sonarqube-%s' % server_version, 'extensions', 'plugins']
+    path = [dst, f"sonarqube-{server_version}", 'extensions', 'plugins']
     path = os.path.join(*path)
     copy_all_files_from_folder(src_of_the_plugins, path)
 
@@ -222,7 +367,6 @@ def main(options):
     start_sq_server(server_version, system, dst)
 
     # 5) Validate the server is started succesfully
-    # 6) Analyze the given project
 
     sleep(60)
     if validate_running_of_sq_server(server_version, noa, wait):
@@ -231,7 +375,60 @@ def main(options):
         print(('SonarQube did not start in time (-noa=%s (number of attempts))' % (noa)))
         if print_log_files:
             print_log(server_version, dst)
-        exit(1)
+        exit(5)
+
+    # 6) Create sq user
+
+    sleep(20)
+    print('Creating user in SonarQube...')
+    if not create_user_in_sq():
+        print('Creating user failed.')
+        exit(6)
+    print('User successfully created.')
+    
+    # 7) Set SourceMeter directory
+
+    print('Setting SourceMeter directory..')
+    if not set_sm(sm_directory):
+        print(f"Could not set SourceMeter directory to: '{sm_directory}'")
+        exit(7)
+    print(f"SourceMeter directory set to: '{sm_directory}'")
+    
+    print('Setting default profile to SourceMeter Way..')
+    if not set_default_profile('SourceMeter Way', language_of_test):
+        print('Setting profile failed.')
+        exit(8)
+    print('Profile set.')
+    
+    # 8) Creating project
+
+    print('Creating project..')
+    token = create_project(test_project_name)
+    if token == None:
+        print('Project creation failed.')
+        exit(9)
+    print('Project successfully created.')
+
+    # 9) Analyzing project with sonar scanner using SM.
+
+    print('Starting analyze project..')
+    analyze(os.path.abspath(dst), 
+            scanner_version, 
+            system, 
+            os.path.abspath('src/sonarqube-core-plugin'), 
+            token,
+            language_of_test,
+            os.path.abspath('src/sonarqube-core-plugin/target/classes'),
+            test_project_name)
+    
+    # 10) Checking the results
+    
+    print('Reading uploaded data from database..')
+    sleep(5) # wait till the results are ready to read.
+    if not validate_results(test_project_name, language_of_test):
+        print('Could not read data or it was not correct.')
+        exit(10)
+    print('Successfully read data.')
 
 if __name__ == "__main__":
     main(get_arguments())
